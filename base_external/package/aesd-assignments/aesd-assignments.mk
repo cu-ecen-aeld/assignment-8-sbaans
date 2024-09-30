51883f209d5ad3a232e8600c3d4e988e636be54b
Sebastien BANON <sebastien.banon@se.com>
2024-09-30 08:31:44 +0200

    change cloning from local to github
    Signed-off-by: Sebastien BANON <sebastien.banon@se.com>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,17 +6,15 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-#AESD_ASSIGNMENTS_VERSION = 60d87b9456c4eda41679f621e0d6aaed62c321b3
+AESD_ASSIGNMENTS_VERSION = f2ebf0dfa8d93a6642433cddb11a9fbcf04dbc22
 
-AESD_ASSIGNMENTS_VERSION = assignment-5-part2
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-#AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sbaans.git
-#AESD_ASSIGNMENTS_SITE_METHOD = git
-#AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
-AESD_ASSIGNMENTS_SITE = /home/sesa214884/GIT/coursera//tes/assignments-3-and-later-sbaans
-AESD_ASSIGNMENTS_SITE_METHOD = local
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-sbaans.git
+AESD_ASSIGNMENTS_SITE_METHOD = git
+AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server

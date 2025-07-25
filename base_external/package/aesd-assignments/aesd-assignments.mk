12b83dd195caa82af57cb6e03ac2208fafbdffc8
sbaans <sebastien.banon@se.com>
2025-07-25 16:25:54 +0200

    fix assignment 8
    Signed-off-by: sbaans <sebastien.banon@se.com>

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 7daeb2c1dfd831099418339d686791217dcba4ca
+AESD_ASSIGNMENTS_VERSION = 97e7266929cd9c74599b80dfad94ebb1ed52ad7f
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
